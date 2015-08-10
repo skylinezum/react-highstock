d0fe51500f8d107c149c114b9b371a0d793332bb
Ken Fehling <kenfehling@gmail.com>
2015-08-10 19:09:42 -0400

    Update Highcharts.jsx

--- src/Highcharts.jsx
+++ src/Highcharts.jsx
@@ -1,43 +1,42 @@
-global.HighchartsAdapter = require('exports?HighchartsAdapter!Highcharts/js/adapters/standalone-framework.src');
-var Highcharts = require('exports?Highcharts!Highcharts');
+var Highstock = require('highstock-browserify');
 var React = require('react');
 var update = require('react/addons').addons.update;
 module.exports = React.createClass({
-  displayName: 'Highcharts',
+    displayName: 'Highstock',
 
-  renderChart: function () {
-    if (!this.props.config) {
-      throw new Error('Config has to be specified, for the Highchart component');
+    renderChart: function () {
+        if (!this.props.config) {
+            throw new Error('Config has to be specified, for the Highchart component');
+        }
+
+        var config = this.props.config;
+        var node = this.refs.chart.getDOMNode();
+
+        if (!config.chart) {
+            config = update(config, {chart: {$set: {}}})
+        }
+
+        config = update(config, {chart: {renderTo: {$set: node}}});
+
+        this.chart = new Highstock.Chart(config);
+    },
+
+    getChart: function() {
+        if (!this.chart) {
+            throw new Error('getChart() should not be called before the component is mounted');
+        }
+        return this.chart;
+    },
+
+    componentDidMount: function () {
+        this.renderChart();
+    },
+    componentDidUpdate: function () {
+        this.renderChart();
+    },
+    render: function () {
+        return <div className="chart" ref="chart" />
     }
-
-    var config = this.props.config;
-    var node = this.refs.chart.getDOMNode();
-
-    if (!config.chart) {
-      config = update(config, {chart: {$set: {}}})
-    }
-
-    config = update(config, {chart: {renderTo: {$set: node}}});
-
-    this.chart = new Highcharts.Chart(config);
-  },
-
-  getChart: function() {
-    if (!this.chart) {
-        throw new Error('getChart() should not be called before the component is mounted');
-    }
-    return this.chart;
-  },
-
-  componentDidMount: function () {
-    this.renderChart();
-  },
-  componentDidUpdate: function () {
-    this.renderChart();
-  },
-  render: function () {
-    return <div className = "chart" ref = "chart" />
-  }
 });
 
-module.exports.Highcharts = Highcharts;
+module.exports.Highstock = Highstock;
