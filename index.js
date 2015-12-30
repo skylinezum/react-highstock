a3d130e368f5a74302549db2dfebcb7dadfab0b8
Jimmy Chan <jimmychan@morrosystems.com>
2015-12-30 12:16:28 -0800

    update high stock and react
    - updated high stock version
    - updated react version

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-var Highstock = require('highstock');
-var React = require('react/addons');
-var update = React.addons.update;
+var Highstock = require('highstock-release');
+var React = require('react');
+var update = require('react-addons-update');
 
 module.exports = React.createClass({
-    displayName: 'Highstock',
+  displayName: 'Highstock',
 
-    renderChart: function () {
-        if (!this.props.config) {
-            throw new Error('Config has to be specified, for the Highchart component');
-        }
-
-        var config = this.props.config;
-        var node = this.refs.chart.getDOMNode();
-
-        if (!config.chart) {
-            config = update(config, {chart: {$set: {}}})
-        }
-
-        config = update(config, {chart: {renderTo: {$set: node}}});
-
-        this.chart = new Highstock.Chart(config);
-    },
-
-    getChart: function() {
-        if (!this.chart) {
-            throw new Error('getChart() should not be called before the component is mounted');
-        }
-        return this.chart;
-    },
-
-    componentDidMount: function () {
-        this.renderChart();
-    },
-    componentDidUpdate: function () {
-        this.renderChart();
-    },
-    render: function () {
-        return React.createElement('div', {className: 'chart', ref:'chart'});
+  renderChart: function() {
+    if (!this.props.config) {
+      throw new Error('Config has to be specified, for the Highchart component');
     }
+
+    var config = this.props.config;
+    var node = this.refs.chart;
+
+    if (!config.chart) {
+      config = update(config, {chart: {$set: {}}});
+    }
+
+    config = update(config, {chart: {renderTo: {$set: node}}});
+
+    this.chart = new Highstock.Chart(config);
+  },
+
+  getChart: function() {
+    if (!this.chart) {
+      throw new Error('getChart() should not be called before the component is mounted');
+    }
+
+    return this.chart;
+  },
+
+  componentDidMount: function() {
+    this.renderChart();
+  },
+
+  componentDidUpdate: function() {
+    this.renderChart();
+  },
+
+  render: function() {
+    return React.createElement('div', {className: 'chart', ref:'chart'});
+  },
+
 });
 
 module.exports.Highstock = Highstock;
