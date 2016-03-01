e84c2d1333f9a670b07be18c9e1e9fa622713f1c
Jimmy Chan <jimmychan@morrosystems.com>
2016-02-29 20:28:20 -0800

    - implement should component update like react-highcharts
    - implement should component update like react-highcharts

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-var ReactFauxDOM = require('react-faux-dom');
 var Highstock = require('highstock-release');
 var React = require('react');
 var update = require('react-addons-update');
 
-class ReactHighstock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.renderChart = this.renderChart.bind(this);
-    this.getChart = this.getChart.bind(this);
-  }
+module.exports = React.createClass({
+  displayName: 'Highstock',
 
-  renderChart() {
-    if (!this.props.config) {
+  propTypes: {
+    config: React.PropTypes.object.isRequired,
+    isPureConfig: React.PropTypes.bool,
+  },
+
+  renderChart: function (config) {
+    if (!config) {
       throw new Error('Config has to be specified, for the Highchart component');
     }
 
-    let conifg = this.props.config;
-    let node = this.refs.chart;
+    var node = this.refs.chart;
 
     if (!config.chart) {
-      config = update(config, {chart: {$set: {}}});
+      config = update(config, { chart: { $set: {} } });
     }
 
-    config = update(config, {chart: {renderTo: {$set: node}}});
+    config = update(config, { chart: { renderTo: { $set: node } } });
 
     this.chart = new Highstock.Chart(config);
-  }
+  },
 
-  getChart() {
+  shouldComponentUpdate: function (newProps) {
+    if (!this.props.isPureConfig || !(this.props.config === newProps.config)) {
+      this.renderChart(newProps.config)
+      return true;
+    }
+
+    return false;
+  },
+
+  getChart: function () {
     if (!this.chart) {
       throw new Error('getChart() should not be called before the component is mounted');
     }
 
     return this.chart;
-  }
+  },
 
-  componentDidMount() {
-    this.renderChart();
-  }
+  componentDidMount: function () {
+    this.renderChart(this.props.config);
+  },
 
-  componentDidUpdate() {
-    this.renderChart();
-  }
+  render: function () {
+    return React.createElement('div', { className: 'chart', ref:'chart' });
+  },
 
-  render() {
-    return React.createElement('div', {className: 'chart', ref:'chart'});
-  }
-}
+});
 
-export default ReactHighstock;
+exports.Highstock = Highstock;
